0d7f447601ffcbe4651ca918f0f8916ae84a2869
Alvaro Borrero <ajhades@hotmail.com>
2020-05-22 20:14:53 -0500

    Agregar popup de asignar presupuesto

--- app/js/main.js
+++ app/js/main.js
@@ -404,6 +404,18 @@ jQuery(document).ready(function ($) {
       return numeral(value).format('$ 0,0[.]00');
     }
   });
+  $("#exCapacitaciones").slider({ id: "slider12c", step: 100000,min: 0, max: 100000000, range: true, value: [9000000, 70000000],formatter: function(value) {
+    return numeral(value[0]).format('$ 0,0[.]00') + ' - '+numeral(value[1]).format('$ 0,0[.]00');
+  } });
+  $("#exPapeleria").slider({ id: "slider12c", step: 100000,min: 0, max: 100000000, range: true, value: [90000000, 70000000] ,formatter: function(value) {
+    return numeral(value[0]).format('$ 0,0[.]00') + ' - '+numeral(value[1]).format('$ 0,0[.]00');
+  } });
+  $("#exInfraestructura").slider({ id: "slider12c", step: 100000,min: 0, max: 100000000, range: true, value: [90000000, 70000000] ,formatter: function(value) {
+    return numeral(value[0]).format('$ 0,0[.]00') + ' - '+numeral(value[1]).format('$ 0,0[.]00');
+  } });
+  $("#exOtros").slider({ id: "slider12c", step: 100000,min: 0, max: 100000000, range: true, value: [90000000, 90000000] ,formatter: function(value) {
+    return numeral(value[0]).format('$ 0,0[.]00') + ' - '+numeral(value[1]).format('$ 0,0[.]00');
+  } });
 
 }); // Close JQuery noConflict
 
