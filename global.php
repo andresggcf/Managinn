3a9e30f55e730d6780a2fbb6ac7145e914e701a4
Andres Guerrero <andresggcf@gmail.com>
2020-05-24 23:20:08 -0500

    Terminando diseño de global.php

--- global.php
+++ global.php
@@ -21,9 +21,52 @@
   include("header.php");
   ?>
 
-<body class="global-view">
+<body class="global-view panel_control presupuesto">
+
+  <div class = "Columna-Perfil d-flex flex-column">
+    <div class="icon_home">
+      <img src="img/iconos/inn.svg" width="180px" alt="" class="">
+    </div>
+    <div class = "Elementos-Perfil">
+      <div class = "Botones-Perfil">
+        <a class="Icono-Menu-Perfil" href = "perfil.php">
+          <img src="./img/iconos/icono_proyectos.svg" alt="Dashboard">
+        </a>
+        <a class="Icono-Menu-Perfil current"  href = "global.php">
+          <img class="current-Icono" src="./img/iconos/icono_global.svg" alt="Global">
+        </a>
+        <a class="Icono-Menu-Perfil " href = "personas.php">
+          <img  src="./img/iconos/icono_personas.svg" alt="Personas">
+        </a>
+        <a class="Icono-Menu-Perfil" href = "presupuesto.php">
+          <img src="./img/iconos/icono_presupuesto.svg" alt="Presupuesto">
+        </a>
+      </div>
+
+      <div class = "Usuario-Perfil"> 
+
+        <img src = "./img/iconos/Icono-Perfil.png" alt="Icono" 
+          height="45px"
+          style = "border-radius: 5px; float: left; margin-right: 10px;">
+
+        <div class = "Perfil-Dropdown-Container" style = "float: right;">
+          <div class="btn-group dropright ">
+            <button type="button" class="btn btn-secondary dropdown-toggle Boton-Dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               
+            </button>
+            <div class="dropdown-menu">
+              <!-- Dropdown menu links -->
+              <a style="color:#eb5757; margin-left: 15px" href = "index.php?logout='1'">Cerrar Sesión</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>  
+
+  <!--
   <div class="NavBar">
-    <div class="Contenedor-Menu">
+    <div class="Contenedor-Menu" style="background-color: none">
       <img src="img/iconos/managinn.png" width="180px" alt="" class="">
     </div>
   </div>
@@ -53,8 +96,8 @@
           style = "border-radius: 5px; float: left; margin-right: 10px;">
 
           <div style = "float:left;">
-            <p class="Texto-Nombre-Perfil"><strong> <?php echo $_SESSION['name_post'];?></strong></p>
-            <p class="Texto-Rol-Perfil"><?php echo $_SESSION['role_post'];?></p>
+            <p class="Texto-Nombre-Perfil"><strong> <?php //echo $_SESSION['name_post'];?></strong></p>
+            <p class="Texto-Rol-Perfil"><?php //echo $_SESSION['role_post'];?></p>
           </div>
 
           <div class = "Perfil-Dropdown-Container" style = "float: right;">
@@ -62,16 +105,16 @@
               <button type="button" class="btn btn-secondary dropdown-toggle Boton-Dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                
               </button>
-              <div class="dropdown-menu">
+              <div class="dropdown-menu">-->
                 <!-- Dropdown menu links -->
-                <a style="color:#eb5757; margin-left: 15px" href = "index.php?logout='1'">Cerrar Sesión</a>
+                <!--<a style="color:#eb5757; margin-left: 15px" href = "index.php?logout='1'">Cerrar Sesión</a>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>  
-  </div>
+  </div> -->
 
 
   
--- presupuesto_informacion.php
+++ presupuesto_informacion.php
@@ -9,57 +9,47 @@
   ?>
 
 <body class="panel_control presupuesto">
-  <div class="NavBar">
-    <div class="Contenedor-Menu">
-      <!-- <img src="img/iconos/managinn.png" width="180px" alt="" class=""> -->
+
+  <div class = "Columna-Perfil d-flex flex-column">
+    <div class="icon_home">
+      <img src="img/iconos/inn.svg" width="180px" alt="" class="">
     </div>
-  </div>
-
-    <div class = "Columna-Perfil d-flex flex-column">
-      <div class="icon_home">
-        <img src="img/iconos/inn.svg" width="180px" alt="" class="">
+    <div class = "Elementos-Perfil">
+      <div class = "Botones-Perfil">
+        <a class="Icono-Menu-Perfil" href = "perfil.php">
+          <img src="./img/iconos/icono_proyectos.svg" alt="Dashboard">
+        </a>
+        <a class="Icono-Menu-Perfil"  href = "global.php">
+          <img   src="./img/iconos/icono_global.svg" alt="Global">
+        </a>
+        <a class="Icono-Menu-Perfil " href = "personas.php">
+          <img  src="./img/iconos/icono_personas.svg" alt="Personas">
+        </a>
+        <a class="Icono-Menu-Perfil current" href = "presupuesto.php">
+          <img class="current-Icono" src="./img/iconos/icono_presupuesto.svg" alt="Presupuesto">
+        </a>
       </div>
-      <div class = "Elementos-Perfil">
-        <div class = "Botones-Perfil">
-          <a class="Icono-Menu-Perfil" href = "perfil.php">
-            <img src="./img/iconos/icono_proyectos.svg" alt="Dashboard">
-          </a>
-          <a class="Icono-Menu-Perfil"  href = "global.php">
-            <img   src="./img/iconos/icono_global.svg" alt="Global">
-          </a>
-          <a class="Icono-Menu-Perfil " href = "personas.php">
-            <img  src="./img/iconos/icono_personas.svg" alt="Personas">
-          </a>
-          <a class="Icono-Menu-Perfil current" href = "presupuesto.php">
-            <img class="current-Icono" src="./img/iconos/icono_presupuesto.svg" alt="Presupuesto">
-          </a>
-        </div>
 
-        <div class = "Usuario-Perfil"> 
+      <div class = "Usuario-Perfil"> 
 
-          <img src = "./img/iconos/Icono-Perfil.png" alt="Icono" 
+        <img src = "./img/iconos/Icono-Perfil.png" alt="Icono" 
           height="45px"
           style = "border-radius: 5px; float: left; margin-right: 10px;">
 
-          <div style = "float:left;">
-            <p class="Texto-Nombre-Perfil"><strong> <?php echo $_SESSION['name_post'];?></strong></p>
-            <p class="Texto-Rol-Perfil"><?php echo $_SESSION['role_post'];?></p>
-          </div>
-
-          <div class = "Perfil-Dropdown-Container" style = "float: right;">
-            <div class="btn-group dropright ">
-              <button type="button" class="btn btn-secondary dropdown-toggle Boton-Dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <div class = "Perfil-Dropdown-Container" style = "float: right;">
+          <div class="btn-group dropright ">
+            <button type="button" class="btn btn-secondary dropdown-toggle Boton-Dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                
-              </button>
-              <div class="dropdown-menu">
-                <!-- Dropdown menu links -->
-                <a style="color:#eb5757; margin-left: 15px" href = "index.php?logout='1'">Cerrar Sesión</a>
-              </div>
+            </button>
+            <div class="dropdown-menu">
+              <!-- Dropdown menu links -->
+              <a style="color:#eb5757; margin-left: 15px" href = "index.php?logout='1'">Cerrar Sesión</a>
             </div>
           </div>
         </div>
       </div>
-    </div>  
+    </div>
+  </div>  
 
     <div class="new-body general_bg">
       <div class="container-fluid">
@@ -283,7 +273,8 @@
       </div>
       <!--container-fluid-->
     </div>
-    <style>
+    
+<style>
   /* Hide all steps by default: */
 .tab {
   display: none;
